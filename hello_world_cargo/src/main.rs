fdf372e35c72c6dc40bac494b484584396ebade6
k1nho <kinhong99@gmail.com>
2022-10-01 17:30:10 -0400

    Using cargo as a package manager

---
+++ hello_world_cargo/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // cargo build (compile and create binary, pass the --release flag to optimize the code)
+    // cargo run (compile and execute)
+    // cargo check (compile, use to check fast for errors)
+    println!("Hello, world!");
+}
