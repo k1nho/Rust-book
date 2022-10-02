39e0b65c0ed037ec906261db12d55374dad04422
k1nho <kinhong99@gmail.com>
2022-10-01 23:29:14 -0400

    Using expressions to return values from functions

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -32,4 +32,21 @@ fn main() {
     let c  = tup.1;
     
     println!("Number is {num}, the decimal is {dec}, and the character is {c}");
+
+    // compound type(array)
+    let arr: [i32; 4] = [1,2,3,4];
+    let arr_sum = sum_array(arr);
+    println!("Sum of the array is {arr_sum}");
+}
+
+fn sum_array(arr : [i32;4]) -> i32 {
+    let mut sum = 0;
+    let mut n = arr.len();
+
+    while n != 0 { 
+        sum += arr[n-1];
+        n -= 1;
+    }
+
+    sum // NOTE return types are expressions rather than statements
 }
