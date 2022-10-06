3d2cb5a3bce1605a8aa85949f00d64bdee162c07
k1nho <kinhong99@gmail.com>
2022-10-06 15:56:18 -0400

    Ownership and functions extended

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -18,4 +18,38 @@ fn main() {
     // time because of this Rust is smart enough to not invalidate x
     // The difference is in the annotation called 'Copy' trait that can be used on types stored in the
     // stack, whereas non-stack ones usually have the 'Drop' trait to deallocate memory
+
+    // Ownership in functions
+    println!("{} {}", is_palindrome(s2), is_palindrome(String::from("racecar"))); 
+    // println!("{}", s2); this code will fail as s2 is relinquished from ownership and the
+    // lifecycle of s2 is part of the function is_palindrome() now
+    
+    let pk_string = create_string(); // ownership is of s from function createString is pk_string
+    let give_string = String::from("give a string");
+    let take_string = process_and_take_string(give_string);
+    
+    // give_string will not work since its ownership was relinquish by process_and_take_string
+    // function
+    println!("pk_string: {}, take_string : {}", pk_string, take_string);
+
+}
+
+fn is_palindrome(s : String) -> bool {
+    let mut l = 0;
+    let mut r = s.len() -1;
+    
+    while l <= r {
+        if s.chars().nth(l) != s.chars().nth(r) { return false;}
+        l += 1; r-=1;
+    }
+    true
+}
+
+fn create_string() -> String {
+    let s = String::from("Pokemon"); // s is owner of the String created
+    s // here ownership is relinquished
+}
+
+fn process_and_take_string(s : String) -> String {
+    s // process and take string ownership of s is relinquished once it returns
 }
