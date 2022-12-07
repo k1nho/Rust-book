62c6acec1e9d8e61b46b9dd8fb531c110dda52ae
k1nho <kinhong99@gmail.com>
2022-12-06 22:09:53 -0500

    test cont

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,7 +1,26 @@
+// useful flags
+// -- --test-threads=1 (run each test in sequence not parallel)
+// -- --show-ouput (show any console log output that is done in the functions)
+// cargo test [name_of_test] to run a single test or tests that match the given parameter
+// [ignore] can be used to ignore an specific test that can take too long
+
 #[derive(Debug)]
 struct Rectangle { 
     width: u32,
     height: u32
+}
+
+struct Guess {
+    value: i32
+}
+
+impl Guess {
+    pub fn new(value : i32) -> Guess {
+        if value > 100 || value <1 {
+            panic!("Guess must in between 1 and 100, got {}", value)
+        }
+        Guess {value}
+    }
 }
 
 impl Rectangle {
@@ -24,9 +43,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn add_two_to_two() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+    
+    #[test]
+    fn add_two_to_three() {
+        let result = add(3, 2);
+        assert_eq!(result, 5);
     }
     
     #[test]
@@ -64,6 +89,12 @@ mod tests {
         let name = "kin";
         let greet = greeting(name);
         assert!(greet.contains("kin"), "Greeting is: {}, did not contain given name: {}", greet, name)
+    }
+
+    #[test]
+    #[should_panic]
+    fn greater_than_100() {
+        Guess::new(1000);
     }
 
 }
