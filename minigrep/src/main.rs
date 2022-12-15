4ad9e5a7f073dd1d12ed69193499c1bdf18c412b
k1nho <kinhong99@gmail.com>
2022-12-15 14:40:07 -0500

    code splitting and error handling

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,15 +1,24 @@
 use std::env;
-use std::fs;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
 
 
     let args : Vec<String> = env::args().collect();
-
-    let query = &args[1];
-    let file_path = &args[2];
     
-    let file_content = fs::read_to_string(file_path).expect("was not able to read file");
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    
+    println!("Searching for: {}",config.query);
+    println!("In file: {}",config.file_path);
+    
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 
-    println!("content of file\n{file_content}");
 }
