2048618a3e2ffc84a16bb532392d3e1c901cc4c5
k1nho <kinhong99@gmail.com>
2022-12-15 16:03:55 -0500

    search and tests

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -21,6 +21,37 @@ impl Config {
 
 pub fn run(config : Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(config.file_path)?;
-    println!("content of file:\n{content}");
+
+    for line in search(&config.query, &content) {
+        println!("{line}")
+    }
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut matches = Vec::new();
+    
+        for line in contents.lines() {
+            if line.contains(query) || line.contains(&query.to_lowercase()) {
+                matches.push(line);
+            }
+        }
+    matches
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+
+    #[test]
+    fn test_match() {
+        let query = "erful";
+        let contents = "\
+Rustlang for the win!
+Powerful, Ownership
+Insane speed, and powerful concurrency";
+
+        assert_eq!(vec!["Powerful, Ownership", "Insane speed, and powerful concurrency"], search(&query, &contents));
+    }
 }
