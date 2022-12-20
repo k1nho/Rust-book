5f83c52591f2472fc88d1727f8502305878adfb9
k1nho <kinhong99@gmail.com>
2022-12-19 22:13:38 -0500

    improving 12 via iterators

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,36 +1,47 @@
+use std::env;
 use std::error::Error;
 use std::fs;
-use std::env;
 
 pub struct Config {
-    pub query : String,
+    pub query: String,
     pub file_path: String,
     pub ignore_case: bool,
 }
 
 impl Config {
-    pub fn build(args : &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        // ignore the executable path which is firt argument
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        let ignore_case =  env::var("IGNORE_CASE").is_ok();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("query was not provided"),
+        };
 
-        Ok(Config { query, file_path , ignore_case})
-    } 
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("filepath was not provided"),
+        };
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
+    }
 }
 
-pub fn run(config : Config) -> Result<(), Box<dyn Error>> {
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(config.file_path)?;
-    
+
     let results = if config.ignore_case {
         search_case_insensitive(&config.query, &content)
     } else {
         search_case_sensitive(&config.query, &content)
     };
-    
+
     for line in results {
         println!("{line}");
     }
@@ -39,32 +50,24 @@ pub fn run(config : Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search_case_sensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut matches = Vec::new();
-    
-        for line in contents.lines() {
-            if line.contains(query) {
-                matches.push(line);
-            }
-        }
-    matches
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
-pub fn search_case_insensitive<'a>(query: &str , contents : &'a str) -> Vec<&'a str> {
-    let mut matches = Vec::new();
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    
-        for line in contents.lines() {
-            if line.to_lowercase().contains(&query) {
-                matches.push(line);
-            }
-        }
-    matches
+
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
 
     #[test]
     fn test_match() {
@@ -74,7 +77,13 @@ Rustlang for the win!
 Powerful, Ownership
 Insane speed, and powerful concurrency";
 
-        assert_eq!(vec!["Powerful, Ownership", "Insane speed, and powerful concurrency"], search_case_sensitive(&query, &contents));
+        assert_eq!(
+            vec![
+                "Powerful, Ownership",
+                "Insane speed, and powerful concurrency"
+            ],
+            search_case_sensitive(&query, &contents)
+        );
     }
 
     #[test]
@@ -87,7 +96,10 @@ Next 13 is out
 Full rust based bundler
 a new next era";
 
-        assert_eq!(vec!["More next to be made", "Next 13 is out", "a new next era"], search_case_insensitive(&query, &contents));
+        assert_eq!(
+            vec!["More next to be made", "Next 13 is out", "a new next era"],
+            search_case_insensitive(&query, &contents)
+        );
     }
 
     #[test]
@@ -100,6 +112,9 @@ Next 13 is out
 Full rust based bundler
 a new next era";
 
-        assert_eq!(vec!["More next to be made", "a new next era"], search_case_sensitive(&query, &contents));
+        assert_eq!(
+            vec!["More next to be made", "a new next era"],
+            search_case_sensitive(&query, &contents)
+        );
     }
 }
