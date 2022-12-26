95d32366b8df1ce8559dbda35ceecfb0dda74085
k1nho <kinhong99@gmail.com>
2022-12-26 13:42:56 -0500

    add content to state trait to enable the return of a post content once it is published

--- oop_rust/src/oop_pattern/post.rs
+++ oop_rust/src/oop_pattern/post.rs
@@ -37,6 +37,9 @@ impl Post {
 trait State {
     fn request_review(self: Box<Self>) -> Box<dyn State>;
     fn approve(self: Box<Self>) -> Box<dyn State>;
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        ""
+    }
 }
 
 struct Draft {}
@@ -70,5 +73,9 @@ impl State for Published {
 
     fn approve(self: Box<Self>) -> Box<dyn State> {
         self
+    }
+
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        &post.content
     }
 }
