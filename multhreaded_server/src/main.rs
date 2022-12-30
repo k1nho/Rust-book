3d5679c5df49dee41b5762d1a2c5a27b1700f772
k1nho <kinhong99@gmail.com>
2022-12-29 20:25:23 -0500

    handle connections with BufReader and process http request

--- multhreaded_server/src/main.rs
+++ multhreaded_server/src/main.rs
@@ -1,10 +1,37 @@
-use std::net::TcpListener;
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("Connection established");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let reader = BufReader::new(&mut stream);
+    let request = reader.lines().next().unwrap().unwrap();
+    //let http_request: Vec<_> = reader
+    //.lines()
+    //.map(|res| res.unwrap())
+    //.take_while(|line| !line.is_empty())
+    //.collect();
+
+    let (status, filename) = if request == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else {
+        ("HTTP/1.1 400 NOT FOUND", "404.html")
+    };
+
+    let content = fs::read_to_string(filename).unwrap();
+    let content_len = content.len();
+    let response = format!("{status}\r\nContent-Length: {content_len}\r\n\r\n{content}");
+    stream.write_all(response.as_bytes()).unwrap();
+
+    //println!("Request: {:#?}", http_request);
 }
